a7f1fff6d0d2dce00e371e5b4bd3550ec4dd2814
Facu185 <facufa12@gmail.com>
2023-11-20 17:41:59 -0300

    finished

--- src/database/db.php
+++ src/database/db.php
@@ -1,16 +1,4 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "gestion_buses";
-/* require_once '../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable("../");
-$dotenv->load();
-$servername = $_ENV['DB_HOST'];
-$username = $_ENV['DB_USERNAME'];
-$password = $_ENV['DB_PASSWORD'];
-$dbname = $_ENV['DB_NAME']; */
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
